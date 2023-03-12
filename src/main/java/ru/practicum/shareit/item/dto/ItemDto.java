ee68906d0e342fc1f4eec66fa0219e0c68c926b7
Gudisa <gudisa.otyrba@yandex.ru>
2023-03-12 12:37:17 +0300

    ShareIt первый вариант

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -8,19 +8,11 @@ import ru.practicum.shareit.request.ItemRequest;
  * TODO Sprint add-controllers.
  */
 @Data
+@Builder
 public class ItemDto {
     private int id;
     private String name;
     private String description;
     private Boolean available;
     private ItemRequest request;
-    
-    @Builder
-    public ItemDto(int id, String name, String description, boolean available, ItemRequest request) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.available = available;
-        this.request = request;
-    }
 }
